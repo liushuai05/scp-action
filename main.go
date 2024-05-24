888b2f0573be0808a254fda357cc9a83490fd682
会PS的小码农 <747357766@qq.com>
2024-05-24 10:12:24 +0800

    zip打包改为golang打包

--- main.go
+++ main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
+	"archive/zip"
 	"bytes"
 	"errors"
 	"fmt"
+	"io"
 	"log"
 	"net"
 	"os"
-	"os/exec"
 	"path"
 	"path/filepath"
 	"strings"
@@ -171,33 +172,38 @@ func Copy(client *ssh.Client) {
 			args = append(args, v)
 		}
 
-		fmt.Println(args)
-		// -x "/dist/.webpackFSCache/*"
-		cmdInstallZip := exec.Command("apt", "-y", "install", "zip")
-		var outzip bytes.Buffer
-		cmdInstallZip.Stdout = &outzip
-		cmdInstallZip.Env = append(os.Environ(),
-			"FOO=duplicate_value", // 重复被忽略
-			"FOO=actual_value",    // 实际被使用
-		)
-		err := cmdInstallZip.Run()
+		err := Zipit(src, dst, exclude)
 		if err != nil {
 			log.Fatalf("❌ Failed to %s file from remote,install zip: %v", os.Getenv("DIRECTION"), err)
 		}
 
-		cmd := exec.Command("zip", args...)
-		// cmd := exec.Command("zip", "-r", dst, src, "-x", "test/1.txt", "-x", "test/2.txt")
-		var out bytes.Buffer
-		cmd.Stdout = &out
-		cmd.Env = append(os.Environ(),
-			"FOO=duplicate_value", // 重复被忽略
-			"FOO=actual_value",    // 实际被使用
-		)
-		err = cmd.Run()
-		if err != nil {
-			log.Fatalf("❌ Failed to %s file from remote: %v", os.Getenv("DIRECTION"), err)
-		}
-		fmt.Printf("Out: %q\n", out.String())
+		// fmt.Println(args)
+		// // -x "/dist/.webpackFSCache/*"
+		// cmdInstallZip := exec.Command("apt", "-y", "install", "zip")
+		// var outzip bytes.Buffer
+		// cmdInstallZip.Stdout = &outzip
+		// cmdInstallZip.Env = append(os.Environ(),
+		// 	"FOO=duplicate_value", // 重复被忽略
+		// 	"FOO=actual_value",    // 实际被使用
+		// )
+		// err := cmdInstallZip.Run()
+		// if err != nil {
+		// 	log.Fatalf("❌ Failed to %s file from remote,install zip: %v", os.Getenv("DIRECTION"), err)
+		// }
+
+		// cmd := exec.Command("zip", args...)
+		// // cmd := exec.Command("zip", "-r", dst, src, "-x", "test/1.txt", "-x", "test/2.txt")
+		// var out bytes.Buffer
+		// cmd.Stdout = &out
+		// cmd.Env = append(os.Environ(),
+		// 	"FOO=duplicate_value", // 重复被忽略
+		// 	"FOO=actual_value",    // 实际被使用
+		// )
+		// err = cmd.Run()
+		// if err != nil {
+		// 	log.Fatalf("❌ Failed to %s file from remote: %v", os.Getenv("DIRECTION"), err)
+		// }
+		// fmt.Printf("Out: %q\n", out.String())
 
 		// 解压测试
 		// if err := UnTar(path, "./un_tar_test"); err != nil {
@@ -303,6 +309,136 @@ func ConfigureHostKeyCallback(expected string, skip string) ssh.HostKeyCallback 
 
 		return nil
 	}
+}
+
+// 压缩为zip格式
+// source为要压缩的文件或文件夹, 绝对路径和相对路径都可以
+// target是目标文件
+// filter是过滤正则(Golang 的 包 path.Match)
+func Zipit(source, target, filter string) error {
+	var err error
+	if isAbs := filepath.IsAbs(source); !isAbs {
+		source, err = filepath.Abs(source) // 将传入路径直接转化为绝对路径
+		if err != nil {
+			return err
+		}
+	}
+	//创建zip包文件
+	zipfile, err := os.Create(target)
+	if err != nil {
+		return err
+	}
+
+	defer func() {
+		if err := zipfile.Close(); err != nil {
+			log.Fatal("❌ Failed to File close error: %s, file: %s", err.Error(), zipfile.Name())
+		}
+	}()
+
+	//创建zip.Writer
+	zw := zip.NewWriter(zipfile)
+
+	defer func() {
+		if err := zw.Close(); err != nil {
+			log.Fatal("❌ Failed to zipwriter close error: %s", err.Error())
+		}
+	}()
+
+	info, err := os.Stat(source)
+	if err != nil {
+		return err
+	}
+
+	var baseDir string
+	if info.IsDir() {
+		baseDir = filepath.Base(source)
+	}
+
+	excludeArr := strings.Split(filter, ",")
+
+	err = filepath.Walk(source, func(path string, info os.FileInfo, err error) error {
+
+		if err != nil {
+			return err
+		}
+
+		//将遍历到的路径与pattern进行匹配
+		ism, err := filepath.Match(filter, info.Name())
+
+		if err != nil {
+			return err
+		}
+		//如果匹配就忽略
+		if ism {
+			return nil
+		}
+		for _, v := range excludeArr {
+			old_v := v
+			isAbs := filepath.IsAbs(v)
+			if !isAbs {
+				v, err = filepath.Abs(v) // 将传入路径直接转化为绝对路径
+				if err != nil {
+					return err
+				}
+			}
+			if v == path {
+				return nil
+			}
+
+			fmt.Println(strings.HasSuffix(old_v, "/") == true, strings.HasPrefix(path, v))
+			fmt.Println(path, v)
+			//判断排除目录
+			if strings.HasSuffix(old_v, "/") == true && strings.HasPrefix(path, v+"/") {
+				return nil
+			}
+
+		}
+
+		//创建文件头
+		header, err := zip.FileInfoHeader(info)
+		if err != nil {
+			return err
+		}
+
+		if baseDir != "" {
+			header.Name = filepath.Join(baseDir, strings.TrimPrefix(path, source))
+		}
+
+		if info.IsDir() {
+			header.Name += "/"
+		} else {
+			header.Method = zip.Deflate
+		}
+		//写入文件头信息
+		writer, err := zw.CreateHeader(header)
+		if err != nil {
+			return err
+		}
+
+		if info.IsDir() {
+			return nil
+		}
+		//写入文件内容
+		file, err := os.Open(path)
+		if err != nil {
+			return err
+		}
+
+		defer func() {
+			if err := file.Close(); err != nil {
+				log.Fatal("❌ Failed to File close error: %s, file: %s", err.Error(), zipfile.Name())
+			}
+		}()
+		_, err = io.Copy(writer, file)
+
+		return err
+	})
+
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 // zip远程解压
